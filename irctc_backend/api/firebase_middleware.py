8421589b09639874c3dcabfd60f8743ebfc5f6c1
Muralidharan Siva <muralidharansiva156@gmail.com>
2026-03-03 12:39:11 +0530

    Remove firebase completely

--- irctc_backend/api/firebase_middleware.py
+++
@@ -1,5 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-if not firebase_admin._apps:
-    firebase_admin.initialize_app()
--- irctc_backend/irctc_backend/settings.py
+++ irctc_backend/irctc_backend/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "api.firebase_middleware.FirebaseAuthMiddleware",
 ]
 
 ROOT_URLCONF = "irctc_backend.urls"
